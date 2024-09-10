5abe04662b4d5b5bcec569a17213ab7b791ba5cf
abdelrahman341 <Abdelrahmanbakir841@gmail.com>
2024-09-10 23:13:51 +0300

    third update

--- employee class phase 1/main.cpp
+++ employee class phase 1/main.cpp
@@ -4,6 +4,7 @@
 #include <employee.h>
 #include "Client.h"
 #include "Validation.h"
+#include <Admin.h>
 
 using namespace std;
 
@@ -11,14 +12,5 @@ using namespace std;
 int main()
 {
 
-
-Client c1(123,"Moatasem", "asd123asd", 2500 );
-employee e1;
-e1.set_id(123);
-e1.set_name("Abdo");
-e1.display_info();
-
-
-c1.display();
     return 0;
 }
